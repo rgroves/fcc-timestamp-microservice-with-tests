c44b9a61815012e34b1166ddd420788065293d68
Robert Groves <rwgdev@gmail.com>
2019-08-04 23:59:32 -0500

    Implemented first requirement.
    For the API endpoint GET /api/timestamp/:date_string, if the date string
    parameter is valid the api should return a JSON having the structure:
    {"unix": <date.getTime()>, "utc" : <date.toUTCString()> }

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 // pull in Express for web server functionality
-const express = require('express')
+const express = require('express');
 // create an instance of Express 
 const app = express();
+
+app.get("/api/timestamp/:date", (req, res) => {
+    const date = new Date(req.params.date);
+    const timestampObj = {'unix': date.getTime(), 'utc': date.toUTCString()};
+    return res.status(200).json(timestampObj);
+});
 
 // Export the express instance.
 module.exports = app;
