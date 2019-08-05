31c72aa04a35869016288cd8905aa86a0fc19dd9
Robert Groves <rwgdev@gmail.com>
2019-08-04 23:39:45 -0500

    Added simple server (no routes defined yet).

---
+++ app.js
@@ -0,0 +1,7 @@
+// pull in Express for web server functionality
+const express = require('express')
+// create an instance of Express 
+const app = express();
+
+// Export the express instance.
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,6 @@
+const app = require('./app');
+
+// start server, listening on port 8080
+app.listen(8080, () => {
+    console.log('app is listening on port 8080');
+});
